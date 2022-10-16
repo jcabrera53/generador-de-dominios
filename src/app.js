ae3360ab2797acaeb8691e661b0a22aab154d666
jcabrera53 <juancabrera53@gmail.com>
2022-10-16 01:30:51 +0000

    generador de dominios básico

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "puedes"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  pronoun.forEach(pronombre => {
+    adj.forEach(adjetivo => {
+      noun.forEach(sustantivo => {
+        console.log(
+          `${sustantivo == "puedes" ? `${pronombre}${adjetivo}pued.es` : ""}`
+        );
+        ext.forEach(extension => {
+          console.log(`
+        ${
+          sustantivo == "puedes" &&
+          (extension == ".com" ||
+            extension == ".net" ||
+            extension == ".us" ||
+            extension == ".io")
+            ? ""
+            : `${pronombre}${adjetivo}${sustantivo}${extension}`
+        }
+        `);
+        });
+      });
+    });
+  });
 };
